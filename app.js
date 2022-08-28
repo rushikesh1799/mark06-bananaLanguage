eab8dd45786dc7b46e06de8672f05c3c0555998d
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2022-08-28 18:21:17 +0530

    Update app.js
    added error handling code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var outputDiv = document.querySelector("#output");
 
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURl(text) {
     return serverURL + "?" + "text=" + text;
@@ -17,21 +17,28 @@ function getTranslationURl(text) {
 // console.log("Hello");
 
 function errorHandler(error) {
-    console.log("error occured", error)
-    alert("something wrong with the server! please try again after some time")
+    console.log("error occured", error);
+    alert("something wrong with the server! please try again after some time");
 }
 
-function handleClick() {
+async function handleClick() {
     // console.log(txtInput.value)
     // outputDiv.innerHTML = txtInput.value
-    fetch(getTranslationURl(txtInput.value))
-        .then(response => response.json())
-        .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText;
-        })
-        .catch(err => console.log(err))
+    if (txtInput.value == "") {
+        alert("Please enter the text/sentence that you want to translate to banana language")
+    } else {
+        try {
+            const res = await fetch(getTranslationURl(txtInput.value));
+            const data = await res.json();
+            if (res.status === 429) {
+                throw new Error(data.error.message);
+            }
+            const translatedText = data.contents.translated;
+            outputText.textContent = translatedText;
+        } catch (err) {
+            alert(err);
+        }
+    }
 }
 
-btnTranslate.addEventListener("click", handleClick)
-
+btnTranslate.addEventListener("click", handleClick);
